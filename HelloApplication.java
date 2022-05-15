efd52c1113cd9dfa944890ba1c91981c8d4b2911
Sajan Pande <87706744+sajan84@users.noreply.github.com>
2022-05-15 17:50:15 +0530

    Create HelloApplication.java

---
+++ HelloApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo2;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button b=new Button("Click me");//leaf control
+        FlowPane fp=new FlowPane();//parent control
+        fp.getChildren().add(b);
+        Scene sc=new Scene(fp );
+        stage.setScene(sc);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+    launch(args);
+    }
+
+
+}
